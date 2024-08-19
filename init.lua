1b1da2c61960904417f096dcb0767f564b0214c6
Alex Kotusenko <olexaus@gmail.com>
2024-08-19 16:43:21 -0400

    first commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- set theme to gruvbox (i installed it in lazy.lua)
+-- -- vim.o.background = "dark" -- or "light" for light mode
+-- -- vim.cmd([[colorscheme gruvbox]])
+-- test theme on startup
+-- vim.cmd([[colorscheme habamax]])
---
+++ lua/plugins/editor.lua
@@ -0,0 +1,35 @@
+return {
+	{
+		"telescope.nvim",
+		priority = 1000,
+		dependencies = {
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+			"nvim-telescope/telescope-file-browser.nvim",
+		},
+		keys = {
+			{
+				"sf",
+				function()
+					local telescope = require("telescope")
+					local function telescope_buffer_dir()
+						return vim.fn.expand("%:p:h")
+					end
+
+					telescope.extensions.file_browser.file_browser({
+						path = "%:p:h",
+						cwd = telescope_buffer_dir(),
+						respect_gitignore = false,
+						hidden = true,
+						grouped = true,
+						previewer = true,
+						initial_mode = "normal",
+						layout_config = { height = 40 },
+					})
+				end,
+			},
+		},
+	},
+}
