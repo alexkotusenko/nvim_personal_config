e17bab294abafff128a6b85d5f7264a590e3090b
Alex Kotusenko <olexaus@gmail.com>
2024-10-08 13:33:31 -0400

    added no bg plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,19 @@
 return {
-	-- add gruvbox
+	-- add schemes
 	{ "ellisonleao/gruvbox.nvim" },
 	{ "EdenEast/nightfox.nvim" },
-	-- Configure LazyVim to load gruvbox
+	{ "rose-pine/neovim" },
+	{ "rebelot/kanagawa.nvim" },
+	{ "kaiuri/nvim-juliana" },
+	{ "sainnhe/gruvbox-material" },
+	-- add no bg plugin
+	{ "xiyaowong/transparent.nvim" },
+	-- Configure LazyVim to load a certain scheme
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "gruvbox",
+			-- this is where you set the colorscheme to be loaded on startup
+			colorscheme = "gruvbox-material",
 		},
 	},
 }
